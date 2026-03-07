a5279d9dd90c377efe76afd1f418f9c053e78c99
curloz123 <curloz@tutamail.com>
2026-03-07 11:27:00 +0530

    Base Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,23 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'LearnVulkan',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Introduction',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Introduction', slug: 'introduction/introduction' },
+						{ label: 'A Brief Introduction to Vulkan', link: 'introduction/vulkan' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Getting Started',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Environment Setup', slug: 'getting_started/env_setup' },
+					],
 				},
 			],
 		}),
