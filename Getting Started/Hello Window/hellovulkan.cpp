9fa3a882aa39f5af6a1b59d35dd20521eb8dac82
curloz123 <curloz@tutamail.com>
2026-03-08 19:17:50 +0530

    Added hello window chapter

--- Getting Started/Hello Window/hellovulkan.cpp
+++ Getting Started/Hello Window/hellovulkan.cpp
@@ -4,10 +4,12 @@
 
 int main()
 {
+    // initialize GLFW
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
+    // create window object
     GLFWwindow *window = glfwCreateWindow(800, 600, "Hello Vulkan", nullptr,nullptr);
     if (window == NULL)
     {
@@ -15,13 +17,23 @@ int main()
         return 1;
     }
 
+    // game loop
     while (!glfwWindowShouldClose(window))
     {
         // Poll all the events
         glfwPollEvents();
+
+        // check whether escape key is pressed or not
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            // If true, set glfwWindowShouldClose to true
+            glfwSetWindowShouldClose(window, true);
+        }
     }
-    
+
+    // cleanup
     glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
