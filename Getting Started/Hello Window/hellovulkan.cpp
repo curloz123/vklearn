b186b42db55eaa85c0038bfbc638dcdfa3e45b87
curloz123 <curloz@tutamail.com>
2026-03-10 20:53:43 +0530

    Added Instance chapter

--- Getting Started/Hello Window/hellovulkan.cpp
+++ Getting Started/Hello Window/hellovulkan.cpp
@@ -4,36 +4,36 @@
 
 int main()
 {
-    // initialize GLFW
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        // initialize GLFW
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    // create window object
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello Vulkan", nullptr,nullptr);
-    if (window == NULL)
-    {
-        fmt::println("Could not initialize window, exiting");
-        return 1;
-    }
-
-    // game loop
-    while (!glfwWindowShouldClose(window))
-    {
-        // Poll all the events
-        glfwPollEvents();
-
-        // check whether escape key is pressed or not
-        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        // create window object
+        GLFWwindow *window = glfwCreateWindow(800, 600, "Hello Vulkan", nullptr,nullptr);
+        if (window == NULL)
         {
-            // If true, set glfwWindowShouldClose to true
-            glfwSetWindowShouldClose(window, true);
+                fmt::println("Could not initialize window, exiting");
+                return 1;
         }
-    }
 
-    // cleanup
-    glfwDestroyWindow(window);
-    glfwTerminate();
+        // game loop
+        while (!glfwWindowShouldClose(window))
+        {
+                // Poll all the events
+                glfwPollEvents();
 
-    return 0;
+                // check whether escape key is pressed or not
+                if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+                {
+                    // If true, set glfwWindowShouldClose to true
+                    glfwSetWindowShouldClose(window, true);
+                }
+        }
+
+        // cleanup
+        glfwDestroyWindow(window);
+        glfwTerminate();
+
+        return 0;
 }
